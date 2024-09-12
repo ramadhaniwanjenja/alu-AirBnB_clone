ad42e16f6921045b3e7f56bda88617563d11a14a
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-09-13 00:12:19 +0300

    updated init

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,28 @@
-"""The __init__ method for models directory"""
-from models.engine import file_storage
+#!/usr/bin/python3
+"""
+This module serves as the initialization file for the models package.
+"""
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-storage = file_storage.FileStorage()
+
+storage = FileStorage()
 storage.reload()
+loaded_objects = storage.all()
+
+
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
+}
